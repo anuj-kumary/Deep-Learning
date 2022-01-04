3d060851915ff2912f7f33e18963602256a2e0e5
anuj-kumary <anujf0510@gmail.com>
2022-01-04 16:55:03 +0530

    feat : Home section completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
-    return (
-        <div>
-            <h1>Welcome to the home page</h1>
+  return (
+    <>
+      <section className="hero">
+        <div className="fluid-container">
+          <div className="left-side">
+            <h3 className="section-title">
+              Premium video meetings. Now free for everyone.
+            </h3>
+            <p className="section-para">
+              We re-engineered the service we build for secure business
+              meetings. The Deaf Duo which is free and available for muted
+              people and deaf people and get productive.
+            </p>
+              <button className="hero-button">Get started</button>
+          </div>
+          <div className="left-side">
+            <div className="hero-img">
+              <img src="images/bg.png" alt="background" />
+            </div>
+          </div>
         </div>
-    )
+      </section>
+    </>
+  );
 }
-
