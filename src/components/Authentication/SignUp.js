a579cc9081db957249ac631e0316ba0ff689f37b
anuj-kumary <anujf0510@gmail.com>
2022-01-04 14:22:48 +0530

    feat:signup component

--- src/components/Authentication/SignUp.js
+++ src/components/Authentication/SignUp.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { FaRocket } from "react-icons/fa";
+
 
 export default function SignUp() {
     return (
-        <div>
-            <h1>Welcome to the sign up page</h1>
-        </div>
+        <>
+            <div className="body">
+                <div className="container">
+                    <div className="box">
+                    <FaRocket className='icon' />
+                        <p className="title">Welcome to the deafDuo</p>
+                        <div className="signup-form">
+                        <input type="text"  id="input-name" placeholder='Name' />
+                        <input type="email"  id="input-email" placeholder='Email' />
+                        <input type="password" id="input-pass" placeholder='Password' />
+                        <button className='sign-in-btn'>Create an account</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
